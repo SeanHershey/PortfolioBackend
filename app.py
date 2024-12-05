29d2b8dea053555a86bde98ee035523cc77c011b
Sean Hershey <sean.p.hershey@gmail.com>
2024-12-04 19:45:31 -1000

    Connect Targon API

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, jsonify, request
+from openai import OpenAI
+import os
+import dotenv
+
 app = Flask(__name__)
+
+dotenv.load_dotenv()
+
+client = OpenAI(
+    base_url="https://api.targon.com/v1", 
+    api_key=os.environ.get("TARGON_API_KEY")
+)
 
 @app.route("/")
 
@@ -7,8 +18,23 @@ app = Flask(__name__)
 def generate():
     data = request.json
     prompt = data.get('prompt')
-    
-    response = jsonify({"response": "Hello World! I am a parrot, " + prompt})
+
+    response = client.chat.completions.create(
+        model="EnvyIrys/EnvyIrys_sn111_14",
+        stream=True,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+
+    text = ""
+
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            text = text + chunk.choices[0].delta.content
+
+    response = jsonify({"response": text})
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
